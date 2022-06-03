831088deaf066420515cfcccf1d5ecf710d85a53
jamik-dev <jamik.oik.group@gmail.com>
2022-06-04 01:29:04 +0500

    oxirgisi shu

--- handlers/users/help.py
+++ handlers/users/help.py
@@ -6,6 +6,5 @@ from loader import dp
 
 @dp.message_handler(CommandHelp())
 async def bot_help(message: types.Message):
-    text = ("Botga rasm yuboring va rasmingiz orqa fonsiz bo'lib qaytadi!")
-    
-    await message.answer("\n".join(text))
+    text = "Botga rasm yuboring va rasmingiz orqa fonsiz bo'lib qaytadi!"
+    await message.answer(text)

0126846f389c4bd9819b8b9151e3ad50d9a08f33
jamik-dev <jamik.oik.group@gmail.com>
2022-06-04 01:20:30 +0500

    bla bla

--- data/config.py
+++ data/config.py
@@ -10,6 +10,6 @@
 # IP = env.str("ip")  # Xosting ip manzili
 
 import os
-BOT_TOKEN=str(os.environ.get("BOT_TOKEN"))
-ADMINS=list(os.environ.get("ADMINS"))
-IP=str(os.environ.get("ip"))
+BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))
+ADMINS = list(os.environ.get("ADMINS"))
+IP = str(os.environ.get("ip"))

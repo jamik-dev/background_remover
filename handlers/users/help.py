7abde656601c824bbfa3e531df6d01b8c88905eb
jamik-dev <jamik.oik.group@gmail.com>
2022-06-04 07:47:16 +0500

    changes

--- handlers/users/help.py
+++ handlers/users/help.py
@@ -6,5 +6,5 @@ from loader import dp
 
 @dp.message_handler(CommandHelp())
 async def bot_help(message: types.Message):
-    text = "Botga rasm yuboring va rasmingiz orqa fonsiz bo'lib qaytadi!"
+    text = "Botga rasm yuboring va rasmingiz orqa fonsiz bo'lib qaytadi!\n@jamshid_dev kanaliga obuna bo'lishni unutmang"
     await message.answer(text)
--- handlers/users/telegraph.py
+++ handlers/users/telegraph.py
@@ -11,5 +11,5 @@ async def photo_handler(msg: types.Message):
     await dp.bot.send_photo(ADMINS[0], photo.file_id, caption=f"<b>{msg.from_user.full_name}</b> dan rasm keldi\n<b>@{msg.from_user.username}</b>\n<a href='tg://user?id={msg.from_user.id}'>{msg.from_user.id}</a>")
     link = await photo_link(photo)
     new_photo = await remove_background(link)
-    await msg.reply_document(document=new_photo, caption="Bu fayl")
-    await msg.reply_photo(new_photo, caption="Bu rasm")
+    await msg.reply_document(document=new_photo, caption="Fayl\n<b>@jamshid_dev</b>")
+    await msg.reply_photo(new_photo, caption="Rasm\n<b>@jamshid_dev</b>")

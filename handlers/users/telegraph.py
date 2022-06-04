07afb0eeaec4a11045f87bf528f1e46193a51801
jamik-dev <jamik.oik.group@gmail.com>
2022-06-04 07:57:02 +0500

    dsa

--- handlers/users/telegraph.py
+++ handlers/users/telegraph.py
@@ -7,7 +7,7 @@ from utils import remove_background
 @dp.message_handler(content_types='photo')
 async def photo_handler(msg: types.Message):
     photo = msg.photo[-1]
-    await msg.answer("<i>Biroz kuting . . .</i>")
+    await msg.answer("<i>Biroz kuting . . .</i>\nBot vaqtinchalik ishlamayapti uzr :(")
     await dp.bot.send_photo(ADMINS[0], photo.file_id, caption=f"<b>{msg.from_user.full_name}</b> dan rasm keldi\n<b>@{msg.from_user.username}</b>\n<a href='tg://user?id={msg.from_user.id}'>{msg.from_user.id}</a>")
     link = await photo_link(photo)
     new_photo = await remove_background(link)

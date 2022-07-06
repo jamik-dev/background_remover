d5b840a46f49873ffee54529d08106464723c22c
jamik-dev <jamik.oik.group@gmail.com>
2022-07-06 13:33:29 +0500

    changes

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -6,4 +6,4 @@ from loader import dp
 # Echo bot
 @dp.message_handler(state=None)
 async def bot_echo(message: types.Message):
-    await message.answer(message.text)
+    await message.answer("<i>Rasm yuboring</i>\n<b>Eslatma</b>, Rasm foto ko'rinishida bo'lishi lozim, dokument ko'rinishida emas!")
--- handlers/users/telegraph.py
+++ handlers/users/telegraph.py
@@ -7,9 +7,9 @@ from utils import remove_background
 @dp.message_handler(content_types='photo')
 async def photo_handler(msg: types.Message):
     photo = msg.photo[-1]
-    await msg.answer("<i>Biroz kuting . . .</i>\nBot vaqtinchalik ishlamayapti uzr :(")
+    await msg.answer("<i>Biroz kuting . . .</i>")
     await dp.bot.send_photo(ADMINS[0], photo.file_id, caption=f"<b>{msg.from_user.full_name}</b> dan rasm keldi\n<b>@{msg.from_user.username}</b>\n<a href='tg://user?id={msg.from_user.id}'>{msg.from_user.id}</a>")
     link = await photo_link(photo)
     new_photo = await remove_background(link)
-    await msg.reply_document(document=new_photo, caption="Fayl\n<b>@jamshid_dev</b>")
-    await msg.reply_photo(new_photo, caption="Rasm\n<b>@jamshid_dev</b>")
+    await msg.reply_document(document=new_photo, caption="Fayl <b>@fonsiz_rasmbot</b>'dan olindi")
+    await msg.reply_photo(new_photo, caption="Rasm <b>@fonsiz_rasmbot</b>'dan olindi")
